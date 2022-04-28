286300956f8cd77b8c48692ac206122d56519194
Alsu <66012600+alsu71@users.noreply.github.com>
2022-04-28 12:26:40 +0300

    Add files via upload

---
+++ Test/Test/SignViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Student on 28.04.2022.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class SignViewController: UIViewController {
+
+    @IBOutlet weak var inputEmail: UITextField!
+    @IBOutlet weak var inputPassword: UITextField!
+    
+    @IBAction func inAction(_ sender: UIButton){
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
